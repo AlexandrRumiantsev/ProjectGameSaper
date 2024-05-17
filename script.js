115b701e41ea48badfc907fb510da46891215eeb
alex <r-sasha@list.ru>
2024-05-17 21:34:30 +0300

    lesson 27

--- script.js
+++ script.js
@@ -20,13 +20,21 @@ const Game = {
 const Record = {
     render: function(){
         root.innerHTML += `
-            <div>Таблица с рекордами</div>
+            <div id='record'>Таблица с рекордами</div>
         `
     }
 }
 
+function test (){
+    alert('test')
+}
+
 const MainField = {
+    startBombCount: 3,
+    isLouse: false,
     render: function(gameLevel){
+
+        MainField.isLouse = false
         console.log('render')
         // Вынесем в  отдельную переменную получение блока main
         const main = document.getElementById('main');
@@ -36,7 +44,7 @@ const MainField = {
             // Используем поле повторно
             this.create(main, () => {
                 this.setFieldClick();
-            })
+            }, gameLevel)
         } else {
             // Создаем поле в первый раз
             const mainBlock = document.createElement('div');
@@ -47,7 +55,7 @@ const MainField = {
                 console.log(this)
                 this.setFieldClick();
                 root.appendChild(mainBlock);
-            });
+            }, gameLevel);
     
         }
 
@@ -57,20 +65,81 @@ const MainField = {
         // Ищем все поля field
         document.querySelectorAll('field')
     },
-    create: function(block, callback){
+    create: function(block, callback, gameLevel){
         // Обнуляем блок с полями
-        block.innerHTML = ''
+        block.innerHTML = '';
+        let bombCounter = 0;
         // Отрисовываем главное поле
         for(let i = 0; i < 25; i++) {
             console.log('Цикл', block)
             let isBomb = Math.floor(Math.random() * 2);
 
-            const field = `
-                <div class='field' data-bomb='${isBomb}'>Поле</div>
-            `
-            block.innerHTML += field
+            const field = document.createElement('div');
+            field.className = 'field';
+            field.onclick = this.clickField
+
+            if(isBomb == 1) {
+                bombCounter++
+                if(gameLevel >= bombCounter) {
+                    field.dataset.bomb = isBomb
+                } else {
+                    field.dataset.bomb = 0
+                }
+               
+
+            } else {
+                field.dataset.bomb = 0
+            }
+  
+            block.appendChild(field);
         }
         callback();
+    },
+    chechBomb: function(callback){
+        // пройдемся по все элментам и откроем бомбы
+        const fileds = document.querySelectorAll('.field');
+        fileds.forEach((field) => {
+            if(field.dataset.bomb == 1) {
+                field.classList.add('bomb');
+            }
+        })
+        callback()
+    },
+    clickField: function(){
+        // Если мы уже проиграли, блокируем функцию
+        if(MainField.isLouse) {
+            return
+        }
+        // Понять, кликнули на бомбу или нет
+        const isBomb = this.dataset.bomb == 1;
+
+        if (isBomb) {
+            
+            document.getElementById('record').innerHTML += `<p>${document.getElementById('points').innerText}</p>`;
+            document.getElementById('points').innerText = '0'
+            MainField.isLouse = true
+
+            MainField.chechBomb(
+                () => {
+                    setTimeout(() => {
+                        // ДЗ TODO Заменить на попап
+                        alert('Вы проиграли!');
+                        // Очистить поле и начать заново
+                        MainField.render(MainField.startBombCount)
+                    }, 500)
+                }
+            )
+          
+
+        } else {
+            this.classList.add('coin');
+            MainField.addCoin()
+        }
+    },
+    addCoin: function(){
+        const nowPoint = document.getElementById('points').innerText;
+        const newPoint = parseInt(nowPoint) + 1;
+        document.getElementById('points').innerText = newPoint
     }
 }
 
@@ -79,16 +148,17 @@ const Menu = {
         root.innerHTML += `
             <div>
                 <button id='start'>Старт</button>
-                <button class='changeLevel' data-level='1'>
+                <button class='changeLevel' data-level='3'>
                     Изи
                 </button>
-                <button class='changeLevel' data-level='2'>
+                <button class='changeLevel' data-level='7'>
                     Лоу
                 </button>
-                <button class='changeLevel' data-level='3'>
+                <button class='changeLevel' data-level='15'>
                     Хай
                 </button>
                 <button id='help'>Глазик</button>
+                <div id='points'>0</div>
             </div>
         `;
         setTimeout( function() {
@@ -101,7 +171,7 @@ const Menu = {
         document.getElementById("start").onclick = function(){
             // alert('Нажали на старт игры')
             // Стартуем с первого уровня
-            MainField.render(1)
+            MainField.render(MainField.startBombCount)
         }
     },
     setEventChangeLevel: function(){
